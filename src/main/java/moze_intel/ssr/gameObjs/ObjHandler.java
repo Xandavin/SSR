730029dfd213633ef7f505a95118a39264ea5f91
The Sarge <sgtpunishment@gmail.com>
2014-11-28 17:45:52 +0000

    Changed Enchantment ID function, prevents conflict
    Removed EnchantmentID config option, should auto-find enchantment id

--- src/main/java/moze_intel/ssr/gameObjs/ObjHandler.java
+++ src/main/java/moze_intel/ssr/gameObjs/ObjHandler.java
@@ -8,19 +8,30 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-public final class ObjHandler
-{
-    public static final Enchantment SOUL_STEALER = new SoulStealerEnchant(SSRConfig.ENCHANT_ID, SSRConfig.ENCHANT_WEIGHT);
-    public static final SSRCreativeTab CREATIVE_TAB = new SSRCreativeTab();
-    public static final Item SOUL_SHARD = new SoulShardItem();
-    public static final Block SOUL_CAGE = new SoulCageBlock();
+public final class ObjHandler {
+	public static Enchantment SOUL_STEALER;
+	public static final SSRCreativeTab CREATIVE_TAB = new SSRCreativeTab();
+	public static final Item SOUL_SHARD = new SoulShardItem();
+	public static final Block SOUL_CAGE = new SoulCageBlock();
 
-    public static void registerObjs()
-    {
-        GameRegistry.registerItem(SOUL_SHARD, "ssr_soul_shard");
-        GameRegistry.registerBlock(SOUL_CAGE, SoulCageItem.class, "ssr_soul_cage");
-        GameRegistry.registerTileEntity(SoulCageTile.class, "ssr_soul_cage_tile");
+	public static void registerObjs() {
+		int counter = 52;
+		boolean found = false;
 
-        GameRegistry.addShapedRecipe(new ItemStack(ObjHandler.SOUL_CAGE), "III", "IXI", "III", 'I', Blocks.iron_bars);
-    }
+		while (counter <= 256 && !found) {
+			if (Enchantment.enchantmentsList[counter] == null) {
+				Enchantment SOUL_STEALER = new SoulStealerEnchant(counter,
+						SSRConfig.ENCHANT_WEIGHT);
+				found = true;
+			}
+		}
+		GameRegistry.registerItem(SOUL_SHARD, "ssr_soul_shard");
+		GameRegistry.registerBlock(SOUL_CAGE, SoulCageItem.class,
+				"ssr_soul_cage");
+		GameRegistry.registerTileEntity(SoulCageTile.class,
+				"ssr_soul_cage_tile");
+
+		GameRegistry.addShapedRecipe(new ItemStack(ObjHandler.SOUL_CAGE),
+				"III", "IXI", "III", 'I', Blocks.iron_bars);
+	}
 }
